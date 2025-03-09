9330ade1651f875064ab4ab68e1ebeaf0529f5b1
Antonio Carlos Souza de Abrantes <otimistarj@gmail.com>
2025-03-09 09:28:45 -0300

    Update 𓂀Egito.py

--- pages/𓂀Egito.py
+++ pages/𓂀Egito.py
@@ -22,10 +22,10 @@ if 'index' not in st.session_state:
 question = df.iloc[st.session_state.index]
 st.markdown(f"<h4>{question['Pergunta']}</h4>", unsafe_allow_html=True)
 
-st.markdown(question['Alternativas'])
+# st.markdown(question['Alternativas'])
 # Exibir alternativas garantindo que seja string
-# options = str(question['Alternativas']).split('\n')
-# selected_option = st.radio("Escolha a alternativa correta:", options)
+options = [alt.strip() for alt in str(question['Alternativas']).split(';') if alt.strip()]
+selected_option = st.radio("Escolha a alternativa correta:", options)
 
 # Mostrar a resposta correta
 if st.button("Mostrar Resposta"):
